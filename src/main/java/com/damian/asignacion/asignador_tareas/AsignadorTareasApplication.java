0ec4d9c5edf6765386a73101994194ec22fb6660
Damian Manchali <dami.manchali.eze@gmail.com>
2025-08-06 21:29:21 -0300

    segundo commit

---
+++ src/main/java/com/damian/asignacion/asignador_tareas/AsignadorTareasApplication.java
@@ -0,0 +1,13 @@
+package com.damian.asignacion.asignador_tareas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsignadorTareasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsignadorTareasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/damian/asignacion/asignador_tareas/AsignadorTareasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.damian.asignacion.asignador_tareas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsignadorTareasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
